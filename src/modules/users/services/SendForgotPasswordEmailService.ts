a1921d9d800544e35f93e84c7f3f74af877fe9df
jean-paulo <jeanprs@msn.com>
2020-08-13 15:29:46 -0300

    Template engine

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,8 +5,6 @@ import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
-// import User from '../infra/typeorm/entities/User';
-
 interface IRequest {
     email: string;
 }
@@ -35,10 +33,20 @@ class SendForgotPasswordEmailService {
         // Gera o token
         const { token } = await this.userTokensRepository.generate(user.id);
 
-        await this.mailProvider.sendMail(
-            email,
-            `Pedido de recuperação de senha recebido. ${token}`,
-        );
+        await this.mailProvider.sendMail({
+            to: {
+                name: user.name,
+                email: user.email,
+            },
+            subject: '[GoBarber] Recuperação de senha',
+            templateData: {
+                template: 'Olá, {{name}}: {{token}}',
+                variables: {
+                    name: user.name,
+                    token,
+                },
+            },
+        });
     }
 }
 
