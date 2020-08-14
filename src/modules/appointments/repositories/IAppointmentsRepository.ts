da3353f2c9f997fe640f04fcd4fda50f44085e39
jean-paulo <jeanprs@msn.com>
2020-08-14 11:17:58 -0300

    Listando horários disponíveis

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,4 +1,5 @@
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -7,5 +8,8 @@ export default interface IAppointmentsRepository {
     findByDate(date: Date): Promise<Appointment | undefined>;
     findAllInMonthFromProvider(
         data: IFindAllInMonthFromProviderDTO,
+    ): Promise<Appointment[]>;
+    findAllInDayFromProvider(
+        data: IFindAllInDayFromProviderDTO,
     ): Promise<Appointment[]>;
 }
