a6cec5645d1d794e367611d1fb0474cfd92092e7
jean-paulo <jeanprs@msn.com>
2020-08-15 07:13:20 -0300

    Agenda do prestador

---
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+export default class ProviderAppointmentsController {
+    public async index(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        // pega o id do usuário que está logado
+        const provider_id = request.user.id;
+        // pega de dentro do request.body os dados que o usuario vai utilizar para criar um novo agendamento
+        const { day, month, year } = request.body;
+
+        const listProviderAppointments = container.resolve(
+            ListProviderAppointmentsService,
+        );
+
+        const appointments = await listProviderAppointments.execute({
+            provider_id,
+            day,
+            month,
+            year,
+        });
+        return response.json(appointments);
+    }
+}
--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,13 +2,16 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 // Como todas as rotas precisam de autenticação, utilizamos o use passando nosso middleware, ele aplica em todas as rotas de agendamentos.
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
