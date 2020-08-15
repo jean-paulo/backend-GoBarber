0326e560cfc8aa84bf8e9064fa5426770140c252
jean-paulo <jeanprs@msn.com>
2020-08-14 22:55:26 -0300

    Regras do agendamento

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,7 +1,7 @@
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import AppError from '@shared/errors/AppError';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -26,6 +26,23 @@ class CreateAppointmentService {
     }: IRequest): Promise<Appointment> {
         const appointmentDate = startOfHour(date);
 
+        if (isBefore(appointmentDate, Date.now())) {
+            throw new AppError(
+                "You can't create an appointment on a past date",
+            );
+        }
+
+        if (user_id === provider_id) {
+            throw new AppError(
+                "You can't create an appointment with yourself.",
+            );
+        }
+
+        if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+            throw new AppError(
+                'You can only create an appointment between 8 am and 5 pm',
+            );
+        }
         // verifica se o appointment está disponivel
         const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
             appointmentDate,
