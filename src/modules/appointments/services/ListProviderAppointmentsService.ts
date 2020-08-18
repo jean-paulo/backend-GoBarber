b2653413ea0ad07cee28ee59e343d1b8d91a8fa3
jean-paulo <jeanprs@msn.com>
2020-08-17 21:52:48 -0300

    Serialização no cache

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { inject, injectable } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -30,6 +31,7 @@ class ListProviderAppointmentsService {
     }: IRequest): Promise<Appointment[]> {
         const cacheKey = `provider-appointments:${provider_id}-${year}-${month}-${day}`;
 
+        // Habilita o cache
         let appointments = await this.cacheProvider.recover<Appointment[]>(
             cacheKey,
         );
@@ -44,7 +46,7 @@ class ListProviderAppointmentsService {
                 },
             );
 
-            await this.cacheProvider.save(cacheKey, appointments);
+            await this.cacheProvider.save(cacheKey, classToClass(appointments));
         }
 
         return appointments;
